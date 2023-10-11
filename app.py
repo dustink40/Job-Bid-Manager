17f211a4e4be77437d6cfc6bae2a4a5d212b8a5f
Dustin Keith <dustinkeith742@gmail.com>
2023-10-10 19:35:37 -0700

    hjy

--- app.py
+++ app.py
@@ -54,12 +54,12 @@ def submit_job():
     onsite_workers = request.form.get('onsite_workers')
     progress = request.form.get('progress')
 
-    print(client_name, contact_info, location, job_details, start_date, estimated_enddate, onsite_workers, progress)
+    print(client_name, contact_info, job_location, job_details, start_date, estimated_enddate, onsite_workers, progress)
 
     # Write to jobs.csv
     with open('jobs.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([client_name, contact_info, location, job_details, start_date, estimated_enddate, onsite_workers, progress])
+        writer.writerow([client_name, contact_info, job_location, job_details, start_date, estimated_enddate, onsite_workers, progress])
 
     return redirect('/')
 
